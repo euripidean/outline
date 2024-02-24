92ec73f9517a180c61e87e64eb94422ac00d4618
Jane Harrison <33559193+euripidean@users.noreply.github.com>
2024-02-23 16:19:07 -0800

    Server rejig is working

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,66 +1,24 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
 //Get all users
 router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ success: true, data: users });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
-  }
-});
+  const uri = process.env.MONGODB_URI;
 
-//Create a new user
-router.post("/users", async (req, res) => {
+  const client = new MongoClient(uri);
   try {
-    const user = await User.create(req.body);
-    res.status(201).json({ success: true, data: user });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
-  }
-});
-
-//Get a single user
-router.get("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    if (!user) {
-      return res.status(404).json({ success: false, error: "User not found" });
-    }
-    res.status(200).json({ success: true, data: user });
+    await client.connect();
+    const database = client.db("outline");
+    const users = database.collection("User");
+    const query = {};
+    const data = await users.find(query).toArray();
+    res.status(200).json({ success: true, data: data });
   } catch (err) {
     res.status(500).json({ success: false, error: err.message });
-  }
-});
-
-//Update a user
-router.put("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).json({ success: false, error: "User not found" });
-    }
-    res.status(200).json({ success: true, data: user });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
-  }
-});
-
-//Delete a user
-router.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).json({ success: false, error: "User not found" });
-    }
-    res.status(200).json({ success: true, data: {} });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
+  } finally {
+    await client.close();
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,21 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const userRoutes = require("./routes/users.js");
+
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
+const PORT = process.env.PORT || 4000;
+
 app.use(express.json());
-// set up all routes
-app.use("/api", require("./routes/users"));
-app.use("/api", require("./routes/projects"));
-app.use("/api", require("./routes/cards"));
-app.use("/api", require("./routes/tags"));
-// get driver connection
-const dbo = require("./db/conn");
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+app.use("/api", userRoutes);
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
